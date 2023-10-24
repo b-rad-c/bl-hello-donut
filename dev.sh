8fb3b5e198cc0264d70854903cff94e448f73d0d
b rad c <bradcorlett@gmail.com>
2023-10-23 23:30:10 -0600

    update for blender 3.6

--- dev.sh
+++ dev.sh
@@ -8,7 +8,7 @@ APP_NAME='bl_hello_donut'
 APP_VERSION='0.0.0'
 
 BL_BIN='/Applications/Blender.app/Contents/MacOS/Blender'
-BL_APP_TEMPLATE_FOLDER='/Users/brad/Library/Application Support/Blender/3.3/scripts/startup/bl_app_templates_user'
+BL_APP_TEMPLATE_FOLDER='/Users/brad/Library/Application Support/Blender/3.6/scripts/startup/bl_app_templates_user'
 BL_APP_TEMPLATE_LINK="${BL_APP_TEMPLATE_FOLDER}/${APP_NAME}"
 
 CURRENT_DIRECTORY=`pwd`
@@ -25,18 +25,27 @@ HELP='i am a help menu. i am not much help. sorry. think of me more as a reminde
 
 MODE="$1"
 
-if [ "$MODE" = "start" ] || [ "$MODE" = "" ]; then
+if [ "$MODE" = "start" ]; then
     clear; 
     PYTHONPATH="${SITE_PACKAGES}:${ADDON_PATH}" "$BL_BIN" --app-template "${APP_NAME}";
 
 elif [ "$MODE" = 'link' ]; then
     ln -s "$APP_SOURCE" "$BL_APP_TEMPLATE_FOLDER"
-    echo -e "linked:\n\t${APP_SOURCE}\n   ->\n\t${BL_APP_TEMPLATE_FOLDER}"
+    
+    if [ $? -eq 0 ]; then 
+        echo -e "linked:\n\t${APP_SOURCE}\n   ->\n\t${BL_APP_TEMPLATE_FOLDER}"
+    else 
+        echo "error linking app, you may need to create intermediate directories"
+    fi
 
 elif [ "$MODE" = 'unlink' ]; then
     unlink "${BL_APP_TEMPLATE_LINK}"
-    echo "unlinked: ${BL_APP_TEMPLATE_LINK}"
-
+    if [ $? -eq 0 ]; then 
+        echo "unlinked: ${BL_APP_TEMPLATE_LINK}"
+    else 
+        echo "error unlinking app"
+    fi
+    
 elif [ "$MODE" = 'package' ]; then
     # remove __pycache__ folders and .DS_Store files
     find $APP_NAME -name __pycache__ -type d -print0 | xargs -0 rm -r --
@@ -51,7 +60,7 @@ elif [ "$MODE" = 'package' ]; then
 
     # zip it all up.
     zip -r $DIST_PATH $APP_NAME
-    echo "\nzipped $APP_NAME to $DIST_PATH"
+    echo -e "\nzipped $APP_NAME to $DIST_PATH"
    
 elif [ "$MODE" = 'help' ]; then
     echo -e "${HELP}"
@@ -59,5 +68,5 @@ elif [ "$MODE" = 'help' ]; then
     echo "${ADDON_PATH}"
 
 else
-    echo -e "error, unknown command: ${MODE}\n${HELP}"
+    echo -e "error, unknown command: ${MODE}\n${HELP}";
 fi
